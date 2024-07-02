381e72eace822763f89e9afd0fe5daa2c8895758
Shruti Mishra <shrutimishra.sm2060@gmail.com>
2024-07-02 18:51:05 +0530

    Update DbReplayAdvice.java

--- mocking-library/agent/src/main/java/com/example/mockinglibrary/agent/DbReplayAdvice.java
+++ mocking-library/agent/src/main/java/com/example/mockinglibrary/agent/DbReplayAdvice.java
@@ -6,12 +6,11 @@ public class DbReplayAdvice {
 
     @Advice.OnMethodEnter
     public static void onEnter(@Advice.Argument(0) String sql) {
-        // Do nothing
     }
 
     @Advice.OnMethodExit
     public static void onExit(@Advice.Return(readOnly = false) Object result) {
         // Replace with hardcoded result
-        result = 1;  // Assuming ID 1 for replay
+        result = 1;
     }
 }
